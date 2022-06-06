37f12ec80f47b300731e6be16e5b0762088966b4
Burhan <burhan.haider@quantumdataengines.com>
2022-06-06 17:30:50 +0530

    role forms changed

--- src/components/common/modules/moduleDataContainer/modulePages/role/RoleComponent.js
+++ src/components/common/modules/moduleDataContainer/modulePages/role/RoleComponent.js
@@ -1,74 +1,187 @@
 import React, { useState, useEffect } from "react";
 import Fab from "@mui/material/Fab";
 // import AddIcon from "@material-ui/icons/Add";
-import { MdAdd as AddIcon } from 'react-icons/md'
-import { GenericDatatable } from "@application";
+import { MdAdd as AddIcon, MdEdit as EditIcon } from 'react-icons/md'
+import { GenericDatatable, GenericDialog } from "@application";
 import RoleCreationForm from "../role/RoleCreationForm";
 import Dialog from "@mui/material/Dialog";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, SpeedDial, SpeedDialAction, SpeedDialIcon, Box } from "@mui/material";
 import roleOperationService from "services/role/RoleOperationService";
+import * as MessageActions from 'redux/message/message.actions'
+import RoleUpdate from "./RoleUpdate";
+import { useDispatch } from "react-redux";
+import { useClasses } from '@application';
+
+const styles = theme => ({
+  speedDial: {},
+  mainDivSpeedDial: {
+    position: "relative",
+    marginRight: "2%",
+    marginBottom: "1%"
+  },
+  speedDialAction: {
+    background: "#d4d4d4"
+  }
+})
 
 function RoleComponent(props) {
   const [open, setOpen] = React.useState(false);
   const [dataSelected, setDataSelected] = useState([]);
-
+  const [openModal, setOpenModal] = useState(false);
+  const [action, setAction] = useState("");
+  const [property, setProperty] = useState({});
   const selectionIndex = "all";
+  const dispatch = useDispatch();
+
+  const classes = useClasses(styles);
   //const selectionIndex = "0,1";
 
-  const openCreaeUserDialog = () => {
-    setOpen(true);
+  const modalData = {
+    AddRole: {
+      title: "Add Role",
+      size: "lg"
+    },
+    UpdateRole: {
+      title: "Update Role",
+      size: "lg"
+    }
   };
-  const closeCreaeUserDialog = () => {
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+  const handleClose = () => {
     setOpen(false);
   };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const displayProperty = data => {
+    setProperty(modalData[data]);
+  };
 
-  function addNewRole(newRoleDetails) {
-    roleOperationService
-      .createNewRole("/role/", newRoleDetails)
-      .then(data => {
-        setOpen(false);
-        props.refreshCurrentModule();
-      })
-      .catch(err => {
-        console.log("Role Operation Service Error:-",err);
-      });
-  }
+  // function addNewRole(newRoleDetails) {
+  //   roleOperationService
+  //     .createNewRole("/role/", newRoleDetails)
+  //     .then(data => {
+  //       setOpen(false);
+  //       props.refreshCurrentModule();
+  //     })
+  //     .catch(err => {
+  //       console.log("Role Operation Service Error:-",err);
+  //     });
+  // }
   return (
-    <div>
-      <GenericDatatable
-        dataSet={props.indexPageData}
-        isSelection={true}
-        isMultipleSelect={false}
-        selectionIndex={selectionIndex}
-        selected={dataSelected}
-        selectHandler={setDataSelected}
-      ></GenericDatatable>
-      <Fab color="primary" aria-label="add">
-        <AddIcon onClick={openCreaeUserDialog} />
-      </Fab>
-      {/*  /> */}
-
-      <Dialog
-        maxWidth="md"
-        fullWidth={true}
-        open={open}
-        onClose={closeCreaeUserDialog}
-        aria-labelledby="form-dialog-title"
-      >
-        <AppBar position="static" elevation={1} className="headerStyle">
-          <Toolbar className="flex w-full">
-            <Typography variant="subtitle1" color="inherit">
-              Add Role
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <RoleCreationForm
-          closeCreaeUserDialog={closeCreaeUserDialog}
-          addNewRole={addNewRole}
-        />
-      </Dialog>
-    </div>
+    <>
+      <div>
+        <GenericDatatable
+          dataSet={props.indexPageData}
+          isSelection={true}
+          isMultipleSelect={false}
+          selectionIndex={selectionIndex}
+          selected={dataSelected}
+          selectHandler={setDataSelected}
+        ></GenericDatatable>
+      </div>
+        {/* <Fab color="primary" aria-label="add">
+          <AddIcon onClick={openCreaeUserDialog} />
+        </Fab> */}
+        {/* <Dialog
+          maxWidth="md"
+          fullWidth={true}
+          open={open}
+          onClose={closeCreaeUserDialog}
+          aria-labelledby="form-dialog-title"
+        >
+          <AppBar position="static" elevation={1} className="headerStyle">
+            <Toolbar className="flex w-full">
+              <Typography variant="subtitle1" color="inherit">
+                Add Role
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <RoleCreationForm
+            closeCreaeUserDialog={closeCreaeUserDialog}
+            addNewRole={addNewRole}
+          />
+        </Dialog> */}
+        <Box classes={classes.mainDivSpeedDial} >
+          <SpeedDial 
+          classes={classes.speedDial}
+          ariaLabel="Role"
+          icon={<SpeedDialIcon />}
+          direction="left"
+          onClose={handleClose}
+          onOpen={handleOpen}
+          open={open}
+          >
+            <SpeedDialAction
+              classes={classes.speedDialAction}
+              key="Add Role"
+              tooltipTitle="Add"
+              tooltipPlacement="top"
+              icon={<AddIcon />}
+              onClick={e => {
+                setAction("Add");
+                handleOpenModal();
+                displayProperty("AddRole");
+              }}
+            />
+            <SpeedDialAction
+              classes={classes.speedDialAction}
+              key="Update Role"
+              tooltipTitle="Update"
+              tooltipPlacement="top"
+              icon={<EditIcon />}
+              onClick={e => {
+                if (dataSelected.length > 0) {
+                  setAction("Update");
+                  handleOpenModal();
+                  displayProperty("UpdateRole");
+                } else {
+                  dispatch(
+                    MessageActions.showMessage({
+                      message: "Please select a record to update",
+                      variant: "warning"
+                    })
+                  );
+                }
+              }}
+              />
+          </SpeedDial>
+          {action === "Add" ? (
+            <GenericDialog 
+              state={openModal}
+              closeModal={handleCloseModal}
+              property={property}
+            >
+              <RoleCreationForm
+                action={action}
+                // addNewRole={addNewRole}
+                closeModal={handleCloseModal}
+              />
+            </GenericDialog>
+          ) : action === "Update" ? (
+            <GenericDialog 
+              state={openModal}
+              closeModal={handleCloseModal}
+              property={property}
+            >
+              <RoleUpdate
+                action={action}
+                selectedData={dataSelected}
+                // addNewRole={addNewRole}
+                closeModal={handleCloseModal} 
+              />
+            </GenericDialog>
+          ) : (
+            ""
+          )}
+        </Box>
+    </>
   );
 }
-
 export default RoleComponent;
--- src/components/common/modules/moduleDataContainer/modulePages/role/RoleCreationForm.js
+++ src/components/common/modules/moduleDataContainer/modulePages/role/RoleCreationForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { FormHelper } from "components/common/FormHelper";
-
 import {
   DialogContent,
   Grid,
@@ -9,20 +8,51 @@ import {
   DialogActions
 } from "@mui/material";
 import { GenericButton } from "@application";
+import { TextFieldFormsy } from "components/common/formsyComponents";
+import roleOperationService from "services/role/RoleOperationService";
+import Formsy from "formsy-react";
+import { useClasses } from '@application'
 
+const styles = theme => ({
+  MuiButton: {
+    textTransform: "initial",
+    marginRight: "1%",
+    borderRadius: "15px",
+    borderColor: "#052a4f",
+    padding: "2px 2.5% 2px 2.5%",
+    "&:hover": {
+      backgroundColor: "#052a4f",
+      color: "white"
+    }
+  },
+  MuiOutlinedInput: {
+    borderRadius: "50px"
+  }
+});
 function RoleCreationForm(props) {
-  const [roleForm, handleChange] = FormHelper({
-    roleName: ""
-  });
-
+  const handleSubmit = newRoleDetails => {
+    roleOperationService
+      .createNewRole("/api/role/", newRoleDetails)
+      .then(data => {
+        // setOpen(false);
+        props.refreshCurrentModule();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    props.closeModal();
+  };
+  const classes = useClasses(styles);
+  // const [roleForm, handleChange] = FormHelper({
+  //   roleName: ""
+  // });
   return (
     <div>
-      <DialogContent>
-        <div className="px-16 sm:px-24">
-          <Grid container spacing={3}>
-            <Grid item xs={6}>
-              <FormControl className="mt-8 mb-16" required fullWidth>
-                <TextField
+      <Formsy onValidSubmit={data => handleSubmit(data)}>
+        <Grid container className="px-16 sm:px-24 mt-3">
+          <Grid item xs={6}>
+                {/* <TextField
+                  fullWidth
                   label="Role Name"
                   autoFocus
                   name="roleName"
@@ -30,31 +60,45 @@ function RoleCreationForm(props) {
                   onChange={handleChange}
                   required
                   variant="outlined"
+                /> */}
+                <TextFieldFormsy 
+                  className="w-5/6"
+                  name="roleName"
+                  label="Role Name"
+                  variant="outlined"
+                  InputProps={{
+                    className: classes.MuiOutlinedInput
+                  }}
                 />
-              </FormControl>
             </Grid>
           </Grid>
-        </div>
-      </DialogContent>
-      <DialogActions className="justify-end pl-8 sm:pl-16">
-        <GenericButton
+          <div
+            className="flex justify-end"
+            style={{
+              marginTop: "2%",
+              marginBottom: "1.5%"
+            }}
+          >
+          <GenericButton
+          type="submit"
           variant="contained"
           color="primary"
-          className="saveButtonStyle"
-          onClick={() => props.addNewRole(roleForm)}
+          className={classes.MuiButton}
+          // onClick={e => props.addNewRole(roleForm)}
+          onClick={e => ""}
         >
           Add
         </GenericButton>
-
         <GenericButton
           variant="contained"
           color="default"
-          className="cancelButtonStyle"
-          onClick={props.closeCreaeUserDialog}
+          className={classes.MuiButton}
+          onClick={props.closeModal}
         >
           Close
         </GenericButton>
-      </DialogActions>
+          </div>
+      </Formsy>
     </div>
   );
 }
